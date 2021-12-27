91b4b2fe0be6d06cfef49df5736ae4c80854f8e2
Muhammad Saqib <saqib.aziz1000@gmail.com>
2021-12-27 12:41:04 +0500

    changes in divider

--- app/components/Divider/styles.js
+++ app/components/Divider/styles.js
@@ -9,12 +9,14 @@ export const styles = StyleSheet.create({
   },
   divider: {
     height: 2,
-    width: '35%',
+    // width: '30%',
+    flex: 1,
     backgroundColor: colors.APP_SECONDARY_COLOR,
   },
   dividertext: {
     color: colors.APP_SECONDARY_COLOR,
     fontSize: 16,
     fontWeight: '400',
+    marginHorizontal: 20,
   },
 });
